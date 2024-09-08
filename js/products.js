425365313e0323da37b1b4d6777c722c1fb28a9c
rominacastro02 <rominica02@gmail.com>
2024-09-08 15:25:02 -0300

    PUNTO-2-DE-LA-ENTREGA-3

--- js/products.js
+++ js/products.js
@@ -1,5 +1,8 @@
 // Array para almacenar los productos
 let listaProductos = [];
+let minPrice = undefined;
+let maxPrice = undefined;
+let currentSortCriteria = undefined;
 
 // Cargar y mostrar los productos al cargar la página
 document.addEventListener('DOMContentLoaded', function() {
@@ -8,10 +11,36 @@ document.addEventListener('DOMContentLoaded', function() {
   getJSONData(PRODUCTS_URL + categoryId + EXT_TYPE).then(function(resultObj){
 
     if (resultObj.status === "ok"){
-      mostrarProductos(resultObj.data.products);
+      listaProductos = resultObj.data.products;
+      mostrarProductos(listaProductos);
     }
   })
 });
+
+ // Añadir evento al botón de filtrado
+ document.getElementById('filterButton').addEventListener('click', function() {
+  minPrice = parseFloat(document.getElementById('minPrice').value);
+  maxPrice = parseFloat(document.getElementById('maxPrice').value);
+  mostrarProductos(listaProductos);
+});
+
+
+
+  // Añadir eventos a los botones de ordenamiento
+  document.getElementById('sortAscPrice').addEventListener('click', function() {
+    currentSortCriteria = "PRICE_ASC";
+    mostrarProductos(listaProductos);
+  });
+
+  document.getElementById('sortPriceDesc').addEventListener('click', function() {
+    currentSortCriteria = 'priceDesc';
+    mostrarProductos(listaProductos);
+  });
+
+  document.getElementById('sortRelevanceDesc').addEventListener('click', function() {
+    currentSortCriteria = 'relevanceDesc';
+    mostrarProductos(listaProductos);
+  });
 
 
 function setProductID(id) {
@@ -20,11 +49,32 @@ function setProductID(id) {
 }
 
 
+// Función para ordenar productos
+function ordenarProductos(productos, criterio) {
+  if (criterio === 'priceAsc') {
+    return productos.sort((a, b) => parseFloat(a.cost) - parseFloat(b.cost));
+  } else if (criterio === 'priceDesc') {
+    return productos.sort((a, b) => parseFloat(b.cost) - parseFloat(a.cost));
+  } else if (criterio === 'relevanceDesc') {
+    return productos.sort((a, b) => b.soldCount - a.soldCount);
+  }
+  return productos;
+}
 
 // Función para mostrar los productos como tarjetas
 function mostrarProductos(productos) {
   let contenedorProductos = document.querySelector('#listaProductos');
   let contenedorHTML = '';
+
+   // Filtrar productos basado en el rango de precios
+   productos = productos.filter(producto => {
+    let precio = parseFloat(producto.cost);
+    return (isNaN(minPrice) || precio >= minPrice) && (isNaN(maxPrice) || precio <= maxPrice);
+  });
+
+  // Ordenar productos basado en el criterio actual
+  productos = ordenarProductos(productos, currentSortCriteria);
+
 
   // Recorrer cada producto y crear su tarjeta
   productos.forEach(producto => {
